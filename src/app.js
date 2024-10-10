5ce0b03701a2800ccfa862af6d5a5d5b483c4c9b
skybanana <112135946+skybanana@users.noreply.github.com>
2024-10-10 20:42:37 +0900

    update login, register

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
 import { loadGameAssets } from './init/assets.js';
-import cors from 'cors'
+import cors from 'cors';
+
+import accountRouter from './routes/account.router.js';
 
 const app = express();
 const server = createServer(app);
@@ -11,21 +13,24 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
+app.use('/api', [accountRouter]);
 app.use(express.urlencoded({ extended: false }));
 initSocket(server);
 
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
 
-  try {
-    const assets = await loadGameAssets();
-    console.log(assets);
-    console.log('Assets loaded successfully');
-  } catch (error) {
-    console.error('Failed to load game assets:', error);
-  }
+  // try {
+  //   const assets = await loadGameAssets();
+  //   console.log(assets);
+  //   console.log('Assets loaded successfully');
+  // } catch (error) {
+  //   console.error('Failed to load game assets:', error);
+  // }
 });
