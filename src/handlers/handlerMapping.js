d9b68d5f95b51ca934c3c95e551299e11f3ed85e
interneton <78246370+interneton@users.noreply.github.com>
2024-10-11 11:07:05 +0900

    handler 맵핑 추가

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,10 +1,18 @@
 import { moveStageHandler } from './stage.handler.js';
 import { gameEnd, gameStart } from './game.handler.js';
+import { purchaseTowerHandler, sellTowerHandler, upgradeTowerHandler } from './tower.handler.js';
+import { killMonsterHandler, spawnMonsterHandler } from './monster.handler.js';
+
 
 const handlerMappings = {
   2: gameStart,
   3: gameEnd,
   11: moveStageHandler,
+  21: purchaseTowerHandler,
+  22: upgradeTowerHandler,
+  23: sellTowerHandler,
+  31: spawnMonsterHandler,
+  32: killMonsterHandler,
 };
 
 export default handlerMappings;
---
+++ src/handlers/monster.handler.js
@@ -0,0 +1,9 @@
+export const spawnMonsterHandler = (userId) => {
+ 
+
+};
+
+export const killMonsterHandler = (userId) => {
+ 
+
+};
