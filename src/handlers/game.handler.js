8b9d3117510d475329eabc67c5ff1c9855c33e0f
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-10-15 10:12:01 +0900

    update : gameStart function

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,18 +1,12 @@
 import { getGameAssets } from '../init/assets.js';
 import { clearStage, getStage, setStage } from '../models/stage.model.js';
 import RedisManager from '../init/redis.js';
+import { gameDataClient, userDataClient } from '../utils/prisma/index.js';
 
 export const gameStart = async (uuid, payload, socket) => {
   const { timeStamp } = payload;
   const { initData } = getGameAssets();
   const { userGold, baseHp } = initData.data;
-
-  if (!timeStamp) {
-    throw new CustomError('게임 초기 정보 검증 실패', 'gameStart');
-  }
-
-  const user = await RedisManager.getCache(uuid);
-  // NOTE: 유저 정보가 없을 때만 초기 정보 넘기기
   const result = {
     status: 'success',
     message: '게임 시작!',
@@ -22,9 +16,17 @@ export const gameStart = async (uuid, payload, socket) => {
     monsterSpawnInterval: 3000,
   };
 
+  if (!timeStamp) {
+    throw new CustomError('게임 초기 정보 검증 실패', 'gameStart');
+  }
+
+  const user = userDataClient.users.findUnique({
+    where: uuid,
+  });
+
   // 저장되어있는 값이 있으면 초기 정보에 덮어 씌우기
   if (user) {
-    result.userGold = user.userGold;
+    result.userGold = user.gold;
     result.baseHp = user.baseHP;
     result.numOfInitialTowers = user.numOfInitialTowers;
   }
