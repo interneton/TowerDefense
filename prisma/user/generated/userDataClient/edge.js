51b75c9b305aaf5c2c511bbeadcd1e11c83ec13d
skybanana <112135946+skybanana@users.noreply.github.com>
2024-10-16 00:30:00 +0900

    prisma generted

--- prisma/user/generated/userDataClient/edge.js
+++ prisma/user/generated/userDataClient/edge.js
@@ -147,7 +147,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../..",
   "clientVersion": "5.20.0",
